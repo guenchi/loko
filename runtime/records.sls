guenchi/loko

commit 74765b1141468f650c2c1b84546795218cee0535

Add support for record writers
This adds the record-writer procedure that works the same as the one
in Chez. (NIH is generally good).

Record writers are also enabled in psyntax so that syntax objects no
longer spam the terminal.

Fixes: #13
Signed-off-by: Göran Weinholt <goran@weinholt.se>

runtime/records.sls
@@ -35,6 +35,7 @@ record-type-descriptors:
   4 uid (symbol or false)
   5 names (vector of symbols)
   6 mutable (bitfield)
+  7 record-writer (procedure)
 
 record-constructor-descriptors:
  -1 'rcd
@@ -55,7 +56,8 @@ record-constructor-descriptors:
     record-accessor record-mutator
     record? record-rtd record-type-name record-type-parent
     record-type-uid record-type-generative? record-type-sealed?
-    record-type-opaque? record-type-field-names record-field-mutable?)
+    record-type-opaque? record-type-field-names record-field-mutable?
+    record-writer)
   (import
     (except (rnrs)
             make-record-type-descriptor record-type-descriptor?
@@ -83,7 +85,7 @@ record-constructor-descriptors:
 (define (make-record-type-descriptor name parent uid sealed? opaque? fields)
   (define who 'make-record-type-descriptor)
   (define (make-rtd flags&len name parent uid names mutable)
-    (let ((ret ($make-box 'rtd 7)))
+    (let ((ret ($make-box 'rtd 8)))
       ($box-set! ret 0 6)
       ($box-set! ret 1 flags&len)
       ($box-set! ret 2 name)
@@ -91,6 +93,7 @@ record-constructor-descriptors:
       ($box-set! ret 4 uid)
       ($box-set! ret 5 names)
       ($box-set! ret 6 mutable)
+      ($box-set! ret 7 default-record-writer)
       ret))
   (define (build parent-length parent-mutable opaque?)
     (let ((len (fx+ (vector-length fields) parent-length)))
@@ -429,7 +432,7 @@ record-constructor-descriptors:
 
 (define (record-type-generative? rtd)
   (assert (record-type-descriptor? rtd))
-  ;; only nongenerative records have an uid
+  ;; only nongenerative records have a uid
   (not ($box-ref rtd 4)))
 
 (define (record-type-sealed? rtd)
@@ -450,4 +453,34 @@ record-constructor-descriptors:
   (let* ((prtd (record-type-parent rtd))
          (k (if prtd (fx+ k ($record-length prtd)) k)))
     (assert (fx<? k ($record-length rtd)))
-    (bitwise-bit-set? ($box-ref rtd 6) k))))
+    (bitwise-bit-set? ($box-ref rtd 6) k)))
+
+;;; Record writers
+
+(define (default-record-writer v p wr)
+  (let ((t (record-rtd v)))
+    (display "#[" p)
+    (display (or (record-type-uid t) (record-type-name t)) p)
+    (let lp ((t t))
+      (when (and t (not (record-type-opaque? t)))
+        (lp (record-type-parent t))
+        (do ((i 0 (fx+ i 1))
+             (fields (record-type-field-names t)))
+            ((fx=? i (vector-length fields)))
+          (let ((field (vector-ref fields i))
+                (value ((record-accessor t i) v)))
+            (display #\space p)
+            (display field p)
+            (display ": " p)
+            (wr value p)))))
+    (display "]" p)))
+
+(define record-writer
+  (case-lambda
+    ((rtd)
+     (assert (record-type-descriptor? rtd))
+     ($box-ref rtd 7))
+    ((rtd procedure)
+     (assert (record-type-descriptor? rtd))
+     (assert (procedure? procedure))
+     ($box-set! rtd 7 procedure)))))

compiler/expander.sls
@@ -1076,6 +1076,7 @@
     (port-file-descriptor-set! ^)       ;for SRFI-170
     (parameterize              ^)       ;Chez-style (not SRFI-39)
     (make-parameter            ^)       ;Chez-style (not SRFI-39)
+    (record-writer             ^)       ;Chez-style record writers
 
     ;; Procedures that cp0 can emit calls to
     (bitwise-lsr)

psyntax/compat.ss
@@ -1,22 +1,22 @@
 ;;; Copyright (c) 2006, 2007 Abdulaziz Ghuloum and Kent Dybvig
-;;; 
+;;;
 ;;; Permission is hereby granted, free of charge, to any person obtaining a
 ;;; copy of this software and associated documentation files (the "Software"),
 ;;; to deal in the Software without restriction, including without limitation
 ;;; the rights to use, copy, modify, merge, publish, distribute, sublicense,
 ;;; and/or sell copies of the Software, and to permit persons to whom the
 ;;; Software is furnished to do so, subject to the following conditions:
-;;; 
+;;;
 ;;; The above copyright notice and this permission notice shall be included in
 ;;; all copies or substantial portions of the Software.
-;;; 
+;;;
 ;;; THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 ;;; IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 ;;; FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
 ;;; THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 ;;; LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 ;;; FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-;;; DEALINGS IN THE SOFTWARE. 
+;;; DEALINGS IN THE SOFTWARE.
 
 (library (psyntax compat)
   (export make-parameter parameterize define-record pretty-print
@@ -28,12 +28,12 @@
           library-stale-warning
           file-locator-resolution-error
           label-binding set-label-binding! remove-location)
-  (import 
+  (import
     (only (loko runtime reader)
           read-annotated annotation? annotation-expression
           annotation-stripped annotation-source
           annotation-source->condition)
-    (only (loko) make-parameter parameterize)
+    (only (loko) make-parameter parameterize record-writer)
     (rnrs)
     (only (psyntax system $bootstrap)
           gensym void eval-core symbol-value set-symbol-value!
@@ -72,31 +72,31 @@
     (display filename p)
     (display "will be recompiled from source.\n" p))
 
-      
+
   (define (file-locator-resolution-error libname failed-list y)
     ;;; please override this in your production implementation
     (error 'file-location "cannot find library" libname failed-list y))
 
-  ;;; we represent records as vectors for portability but this is 
+  ;;; we represent records as vectors for portability but this is
   ;;; not nice.  If your system supports compile-time generative
-  ;;; records, replace the definition of define-record with your 
-  ;;; system supplied definition (which you should support in the 
+  ;;; records, replace the definition of define-record with your
+  ;;; system supplied definition (which you should support in the
   ;;; expander first of course).
-  ;;; if your system allows associating printers with records, 
-  ;;; a printer procedure is provided (so you can use it in the 
-  ;;; output of the macro).  The printers provided take two 
-  ;;; arguments, a record instance and an output port.  They 
-  ;;; output something like #<stx (foo bar)> or #<library (rnrs)> 
+  ;;; if your system allows associating printers with records,
+  ;;; a printer procedure is provided (so you can use it in the
+  ;;; output of the macro).  The printers provided take two
+  ;;; arguments, a record instance and an output port.  They
+  ;;; output something like #<stx (foo bar)> or #<library (rnrs)>
   ;;; to the port.
   ;;;
   ;;; The following should be good for full R6RS implementations.
   ;;;
   ;;;   (define-syntax define-record
   ;;;     (syntax-rules ()
-  ;;;       ((_ name (field* ...) printer) 
+  ;;;       ((_ name (field* ...) printer)
   ;;;        (define-record name (field* ...)))
   ;;;       ((_ name (field* ...))
-  ;;;        (define-record-type name 
+  ;;;        (define-record-type name
   ;;;           (sealed #t)     ; for better performance
   ;;;           (opaque #t)     ; for security
   ;;;           (nongenerative) ; for sanity
@@ -122,6 +122,7 @@
         [(_ name (field* ...) printer)
          #`(begin
              (define-record name (field* ...))
+             (define rp (record-writer (record-type-descriptor name) printer))
              #;
              (define rp (make-record-printer 'name printer)))]
         [(_ name (field* ...))
@@ -136,7 +137,7 @@
                #;(nongenerative) ; for sanity
                (fields (mutable field* getter* setter*) ...)))])))
 
-  (define (file-options-spec x) 
+  (define (file-options-spec x)
     (error 'file-options-spec "not implemented"))
 
 )

psyntax/library-manager.ss
@@ -63,12 +63,12 @@
     (lambda (x p wr)
       (unless (library? x)
         (assertion-violation 'record-type-printer "not a library"))
-      (display
-        (format "#<library ~s>"
-          (if (null? (library-version x))
+      (display "#<library " p)
+      (wr (if (null? (library-version x))
               (library-name x)
-              (append (library-name x) (list (library-version x)))))
-        p)))
+              (append (library-name x) (list (library-version x))))
+          p)
+      (display ">" p)))
 
   (define (library-invoke-dependencies lib)
     (library-inv* lib))

runtime/io.sls
@@ -131,6 +131,7 @@
     (prefix (only (rnrs) eof-object eof-object?) sys:)
     (only (loko runtime arithmetic) $display-number)
     (only (loko runtime symbols) $gensym-generate-names!)
+    (only (loko runtime records) record-writer)
     (loko system $primitives))
 
 ;; Tracing this only works when $debug-put-u8 is used directly.
@@ -1386,21 +1387,8 @@
                        (let ((symbol-name ($box-ref v 0)))
                          (display (utf8->string symbol-name) p))))))
              ((record-type-descriptor? t)
-              (display "#[" p)
-              (display (or (record-type-uid t) (record-type-name t)) p)
-              (let lp ((t t))
-                (when (and t (not (record-type-opaque? t)))
-                  (lp (record-type-parent t))
-                  (do ((i 0 (fx+ i 1))
-                       (fields (record-type-field-names t)))
-                      ((fx=? i (vector-length fields)))
-                    (let ((field (vector-ref fields i))
-                          (value ((record-accessor t i) v)))
-                      (display #\space p)
-                      (display field p)
-                      (display ": " p)
-                      (write value p)))))
-              (display "]" p))
+              (let ((writer (record-writer t)))
+                (writer v p write)))
              (else
               (display "#<box " p)
               (if ($box-header? t)

commit fca1a4a28b255139e1b31cade0baa95d7aa7820e

Fix bug introduced in 74765b11
The extra field in the rtd was not matched by an increase in the
length field, so the GC would not copy the whole rtd.

Fixes: #18
Signed-off-by: Göran Weinholt <goran@weinholt.se>

runtime/records.sls
@@ -86,7 +86,7 @@ record-constructor-descriptors:
   (define who 'make-record-type-descriptor)
   (define (make-rtd flags&len name parent uid names mutable)
     (let ((ret ($make-box 'rtd 8)))
-      ($box-set! ret 0 6)
+      ($box-set! ret 0 7)
       ($box-set! ret 1 flags&len)
       ($box-set! ret 2 name)
       ($box-set! ret 3 parent)